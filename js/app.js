2673ce9bf8e519fe7fb36ab119340a0b49a0b131
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-03-11 02:10:37 +0600

    done like button

--- js/app.js
+++ js/app.js
@@ -16,8 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  console.log(id)
-    likedPostsId.plus(id); 
+    likedPostsId.push(id); 
     showPosts(posts);
 };
 
@@ -149,6 +148,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).innerHTML = '';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
